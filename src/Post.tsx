e6434e12c1e030c66940b46b1038d52f563aef8d
Yaroslav <fil.i.al4ik@gmail.com>
2024-11-25 15:30:58 +0100

    domashka ya 24.11

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,17 +1,44 @@
+import { useState } from "react"
+
 interface PostProps {
     title: string
     description: string
     imageUrl: string
     author: string 
   }
-  
   export function Post(props: PostProps) {
+
+
+    const [likes, setLikes] = useState(0)
+    const [isLiked, setIsLiked] = useState(false)
+    function checkLike() {
+      if (!isLiked) {
+        setLikes(likes + 1)
+        setIsLiked(true)
+      }
+    }
+
+
     return (
       <div>
         <h1>{props.title}</h1>
         <p>{props.description}</p>
-        <img src={props.imageUrl} alt={props.title} />
+        <img src={props.imageUrl}/>
         <p>Автор: {props.author}</p>
+        
+
+        <p>Лайки: {likes}</p>
+        <button onClick={checkLike} disabled={isLiked}>
+          {(() => {
+            if (isLiked) {
+              return "Лайк поставлен"
+            } else {
+              return "Поставить лайк"
+            }
+          })()}
+        </button>
+
+
       </div>
     )
   }
